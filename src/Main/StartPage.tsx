867362bb13cec8d26f29d73148220523f0c15a71
merisaar <mtmsaa@utu.fi>
2021-10-03 22:11:54 +0300

    Initialize react ts web project with prettier and eslint

---
+++ src/Main/StartPage.tsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect, FunctionComponent, ReactComponentElement, ReactElement } from 'react';
+
+export const StartPage = (): any => {
+    const test = "";
+    return (
+        <>
+            This is a test
+        </>
+    )
+}
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { StartPage } from './Main/StartPage';
+
+ReactDOM.render(
+  <>
+    <h1>Web app page</h1>
+    <StartPage />
+  </>,
+  document.getElementById('root')
+);
