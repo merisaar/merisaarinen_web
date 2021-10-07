901145674eb745c8d5331cf7368833deb1f2c20d
merisaar <mtmsaa@utu.fi>
2021-10-07 23:28:33 +0300

    Add design changes to the front page

--- src/Main/StartPage.tsx
+++ src/Main/StartPage.tsx
@@ -11,13 +11,12 @@ export const StartPage = (): any => {
         <>
             <div className="flex-container">
                 <div className="main-left-container startpage-column-container">
-                    <div>test</div>
                     <div className="left-text-container">
                         <div className="main-title flex-item"><p>Hello.<br></br>I am Meri.</p></div>
                         <div className="secondary-title flex-item">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
-                        <div id="show-projects-link flex-item"><Link to="/projects">Show projects<i className="fa fa-arrow-right"></i></Link></div>
+                        <div id="show-projects-link"><Link to="/projects">Show projects<i className="fa fa-arrow-right"></i></Link></div>
                     </div>
-                    <div>test</div>
+                    <div className="social-media-link-container"></div>
                 </div>
                 <div className="main-right-container"><img className="half-image" src="../Resources/startpage-image.jpg"></img></div>
             </div>
