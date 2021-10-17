60687788ceba6b42281fff2307ccd0b765e86a69
merisaar <mtmsaa@utu.fi>
2021-10-17 20:50:52 +0300

    Redirect to home if path is not found

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { MainPage } from './Main/MainPage';
 import { Footer } from 'rsuite';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import 'rsuite/styles/index.less';
 import './Styling/common.less';
 import { UnderWorkPage } from './General_components/UnderWorkPage';
@@ -11,12 +11,11 @@ ReactDOM.render(
     <>
         <Router>
             <Switch>
-                <Route exact path="/about">
-                    {<UnderWorkPage />}
-                </Route>
+                <Route path="/about">{<UnderWorkPage />}</Route>
                 <Route path="/users">{<UnderWorkPage />}</Route>
                 <Route path="/projects">{<UnderWorkPage />}</Route>
                 <Route path="/">{<MainPage />}</Route>
+                <Redirect path="*" to="/" />
             </Switch>
         </Router>
         <Footer></Footer>
