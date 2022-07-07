a056107294cc4955b40b8f67608897712fc66df5
Meri Saarinen <mtmsaa@utu.fi>
2022-07-07 23:29:08 +0300

    Add animation to the home page

--- src/Main/Sections/Home.tsx
+++ src/Main/Sections/Home.tsx
@@ -3,6 +3,7 @@ import { HashLink } from 'react-router-hash-link';
 import '../../Styling/home.less';
 import { HalfImage } from './HalfImage';
 import { Breakpoint } from 'react-socks';
+import Particles from 'react-tsparticles';
 
 export const Home = (): JSX.Element => {
     return (
@@ -27,23 +28,108 @@ export const Home = (): JSX.Element => {
 };
 
 const HomeTextContainer = (): JSX.Element => (
-    <div className="left-text-container fade-in-text-container">
-        <div className="main-title flex-item">
-            <p>
-                Hello.<br></br>I am Meri.
-            </p>
-        </div>
-        <Breakpoint lassName="secondary-title flex-item" small down>
-            <p>I am a software developer that loves trying out new technologies and solving problems through code.</p>
-        </Breakpoint>
-        <Breakpoint lassName="secondary-title flex-item" medium up>
-            <p>I am a software developer that loves trying out new technologies</p>
-            <p>and solving problems through code.</p>
-        </Breakpoint>
-        <div className="startpage-link">
-            <HashLink to="/about">
-                More about me <i className="fa fa-arrow-right"></i>
-            </HashLink>
+    <div>
+        <Particles
+            options={{
+                fullScreen: {
+                    enable: false,
+                    zIndex: 0,
+                },
+                fpsLimit: 120,
+                interactivity: {
+                    detectsOn: 'canvas',
+                    events: {
+                        onHover: {
+                            enable: true,
+                            mode: 'attract',
+                        },
+                        resize: true,
+                    },
+                    modes: {
+                        bubble: {
+                            distance: 400,
+                            duration: 2,
+                            opacity: 1,
+                            size: 40,
+                        },
+                        push: {
+                            quantity: 4,
+                        },
+                    },
+                },
+                particles: {
+                    number: {
+                        value: 30,
+                        density: {
+                            enable: true,
+                        },
+                    },
+                    color: {
+                        value: ['#610909', '#975050', '#ba7d7d'],
+                    },
+                    shape: {
+                        type: 'circle',
+                    },
+                    opacity: {
+                        value: 0.2,
+                        random: {
+                            enable: true,
+                            minimumValue: 0.4,
+                        },
+                        animation: {
+                            enable: false,
+                            speed: 0.25,
+                            minimumValue: 0.1,
+                            sync: false,
+                        },
+                    },
+                    size: {
+                        value: 50,
+                        random: {
+                            enable: true,
+                            minimumValue: 30,
+                        },
+                        animation: {
+                            enable: true,
+                            speed: 10,
+                            minimumValue: 10,
+                            sync: false,
+                        },
+                    },
+                    move: {
+                        enable: true,
+                        speed: 1,
+                        direction: 'top',
+                        random: false,
+                        straight: false,
+                        outModes: {
+                            default: 'out',
+                        },
+                    },
+                },
+                detectRetina: false,
+            }}
+        />
+        <div className="left-text-container fade-in-text-container">
+            <div className="main-title flex-item">
+                <p>
+                    Hello.<br></br>I am Meri.
+                </p>
+            </div>
+            <Breakpoint lassName="secondary-title flex-item" small down>
+                <p>
+                    I am a software developer that loves trying out new technologies and solving problems through code.
+                </p>
+            </Breakpoint>
+            <Breakpoint lassName="secondary-title flex-item" medium up>
+                <p>I am a software developer that loves trying out new technologies</p>
+                <p>and solving problems through code.</p>
+            </Breakpoint>
+            <div className="startpage-link">
+                <HashLink to="/about">
+                    More about me <i className="fa fa-arrow-right"></i>
+                </HashLink>
+            </div>
         </div>
     </div>
 );
