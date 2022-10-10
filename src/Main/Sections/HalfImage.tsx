6611d393e3a1762bec4d5b6251d20037f5b8b2af
Meri Saarinen <mtmsaa@utu.fi>
2022-10-10 21:56:03 +0300

    Set height to full on start page

--- src/Main/Sections/HalfImage.tsx
+++ src/Main/Sections/HalfImage.tsx
@@ -2,17 +2,21 @@ import React, { useState } from 'react';
 
 interface HalfImageProps {
     image: HTMLImageElement;
+    imageRef?: React.RefObject<HTMLImageElement>;
 }
 
-export const HalfImage = ({ image }: HalfImageProps): JSX.Element => {
+export const HalfImage = ({ image, imageRef }: HalfImageProps): JSX.Element => {
     const [loaded, setLoaded] = useState(false);
     return (
-        <img
-            className="half-image"
-            style={loaded ? {} : { display: 'none' }}
-            src={image}
-            onLoad={() => setLoaded(true)}
-            alt=""
-        />
+        <div className="full-height">
+            <img
+                className="half-image"
+                style={loaded ? {} : { display: 'none' }}
+                src={image}
+                onLoad={() => setLoaded(true)}
+                alt=""
+                ref={imageRef}
+            />
+        </div>
     );
 };
